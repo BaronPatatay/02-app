56dc37f4937687412efda9299f84237358c58781
Krawczyk <dawid.krawczyk@capgemini.com>
2020-06-22 11:53:47 +0200

    update

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace _02_app
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello Azure!");
                 });
             });
         }
